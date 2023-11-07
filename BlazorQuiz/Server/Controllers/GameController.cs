2f87fb2a8321285a07db51be6341f3ced47125ad
Wettergrund <Jonas.wettergrund@gmail.com>
2023-11-07 11:45:21 +0100

    Added controllers

---
+++ BlazorQuiz/Server/Controllers/GameController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorQuiz.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetGame(int id)
+        {
+            // Get a specific game
+            return Ok();
+        }
+        [HttpPost("create/{title}")]
+        public IActionResult PostQuiz(string title)
+        {
+
+            //Create a new quiz
+            return Ok();
+        }
+
+        [HttpPost("create/{gameId}")]
+        public IActionResult PostNewGame(int gameId)
+        {
+
+            //Create a new game.
+            //Use Identity to bind to creator user
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateGame (int gameId, string gameState)
+        {
+            // Update game state + return guess result.
+            return Ok();
+        }
+
+
+
+
+
+
+    }
+}
---
+++ BlazorQuiz/Server/Controllers/ProfileController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorQuiz.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        [HttpGet("games")]
+        public IActionResult GetUserGames()
+        {
+            // Return games related to userId (Use identity to fetch ID)
+
+            return Ok();
+        }
+        [HttpGet("mygames")]
+        public IActionResult GetCreatedGames(int id)
+        {
+            // Return quizes created by user (Use identity to fetch ID)
+
+            return Ok();
+        }
+    }
+}
