9112bb6262049ebab3d2a1b94dc38a79e08fdb2a
Daniel Berkowicz <daniel.berkowicz1@gmail.com>
2023-11-08 16:02:55 +0100

    removed some boilerplate and added not finished createquizz and game components

---
+++ BlazorQuiz/Client/Models/QuizModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorQuiz.Client.Models
+{
+    public class QuizModel
+    {
+        public string QuizImageUrl { get; set; }
+        public string Question { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
+        public string CorrectAnswer { get; set; }
+    }
+}
--- BlazorQuiz/Client/Program.cs
+++ BlazorQuiz/Client/Program.cs
@@ -1,27 +1,29 @@
-using BlazorQuiz.Client;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 namespace BlazorQuiz.Client
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            builder.RootComponents.Add<HeadOutlet>("head::after");
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("#app");
+			builder.RootComponents.Add<HeadOutlet>("head::after");
+			builder.Services.AddMudServices();
+			builder.Services.AddBlazoredLocalStorage();
 
-            builder.Services.AddHttpClient("BlazorQuiz.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+			builder.Services.AddHttpClient("BlazorQuiz.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+				.AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
-            // Supply HttpClient instances that include access tokens when making requests to the server project
-            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorQuiz.ServerAPI"));
+			// Supply HttpClient instances that include access tokens when making requests to the server project
+			builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorQuiz.ServerAPI"));
+			builder.Services.AddApiAuthorization();
 
-            builder.Services.AddApiAuthorization();
-
-            await builder.Build().RunAsync();
-        }
-    }
+			await builder.Build().RunAsync();
+		}
+	}
 }
