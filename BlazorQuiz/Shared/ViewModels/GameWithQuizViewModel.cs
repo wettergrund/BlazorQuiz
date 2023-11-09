7fdb50b6db67c5322c09d2753c3798a9e2c324f3
Jonas Wettergrund <jonas.wettergrund@gmail.com>
2023-11-09 11:10:35 +0100

    Separated Http request handling and db requests for GameController

---
+++ BlazorQuiz/Shared/ViewModels/GameWithQuizViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorQuiz.Server.Models
+{
+    public class GameWithQuizViewModel
+    {
+        // Properties from UserQuizModel
+        public int UserQuizId { get; set; }
+        public int Score { get; set; }
+
+        // Properties from QuizModel
+        public int QuizId { get; set; }
+        public Guid QuizPublicId { get; set; }
+        public string QuizName { get; set; }
+        public int QuizTimer { get; set; }
+        public ICollection<> Questions { get; set; }
+
+    }
+}
