0424e7891369d7b995da44cf914722efcc09edc3
Daniel Berkowicz <daniel.berkowicz1@gmail.com>
2023-11-10 10:43:23 +0100

    removed component

--- BlazorQuiz/Server/Controllers/ProfileController.cs
+++ BlazorQuiz/Server/Controllers/ProfileController.cs
@@ -1,43 +1,40 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BlazorQuiz.Server.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Identity;
-using BlazorQuiz.Server.Data;
 using System.Security.Claims;
-using BlazorQuiz.Server.Services;
 
 namespace BlazorQuiz.Server.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProfileController : BaseController
-    {
-    
-        private readonly IProfileService _profileService;
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProfileController : BaseController
+	{
 
-        public ProfileController(IProfileService profileService)
-        {
- 
-            _profileService = profileService;
-        }
+		private readonly IProfileService _profileService;
 
-        [HttpGet("mygames")]
-        public async Task<IActionResult> GetUserGames()
-        {
-            // Return games related to userId (Use identity to fetch ID)
+		public ProfileController(IProfileService profileService)
+		{
 
-            var userGames = await _profileService.GetUserGamesAsync(UserId);
+			_profileService = profileService;
+		}
 
-            return Ok(userGames);
-        }
-        [HttpGet("myquizzes")]
-        public async Task<IActionResult> GetCreatedGames()
-        {
-            // Return quizes created by user (Use identity to fetch ID)
-            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value; //Get user ID from header
+		[HttpGet("mygames")]
+		public async Task<IActionResult> GetUserGames()
+		{
+			// Return games related to userId (Use identity to fetch ID)
 
-            var userQuizzes = await _profileService.GetUserCreatedGamesAsync(UserId);
+			var userGames = await _profileService.GetUserGamesAsync(UserId);
 
-            return Ok(userQuizzes);
-        }
-    }
+			return Ok(userGames);
+		}
+		[HttpGet("myquizzes")]
+		public async Task<IActionResult> GetCreatedGames()
+		{
+			// Return quizes created by user (Use identity to fetch ID)
+			var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value; //Get user ID from header
+
+			var userQuizzes = await _profileService.GetUserCreatedGamesAsync(UserId);
+
+			return Ok(userQuizzes);
+		}
+	}
 }
