6986bfa5b50059c14aa1004df9a68999c049bf8c
Daniel Berkowicz <daniel.berkowicz1@gmail.com>
2023-11-10 09:10:47 +0100

    Added som fronend components, tbc

--- BlazorQuiz/Client/Program.cs
+++ BlazorQuiz/Client/Program.cs
@@ -6,24 +6,26 @@ using MudBlazor.Services;
 
 namespace BlazorQuiz.Client
 {
-	public class Program
-	{
-		public static async Task Main(string[] args)
-		{
-			var builder = WebAssemblyHostBuilder.CreateDefault(args);
-			builder.RootComponents.Add<App>("#app");
-			builder.RootComponents.Add<HeadOutlet>("head::after");
-			builder.Services.AddMudServices();
-			builder.Services.AddBlazoredLocalStorage();
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+            builder.Services.AddMudServices();
+            builder.Services.AddBlazoredLocalStorage();
+            // Register IHttpContextAccessor in the dependency injection container
+            builder.Services.AddHttpContextAccessor();
 
-			builder.Services.AddHttpClient("BlazorQuiz.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-				.AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+            builder.Services.AddHttpClient("BlazorQuiz.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
-			// Supply HttpClient instances that include access tokens when making requests to the server project
-			builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorQuiz.ServerAPI"));
-			builder.Services.AddApiAuthorization();
+            // Supply HttpClient instances that include access tokens when making requests to the server project
+            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorQuiz.ServerAPI"));
+            builder.Services.AddApiAuthorization();
 
-			await builder.Build().RunAsync();
-		}
-	}
+            await builder.Build().RunAsync();
+        }
+    }
 }
