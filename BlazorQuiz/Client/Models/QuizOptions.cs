1469d2fca337d4f00099fa4c98067db2040893c2
Jonas Wettergrund <jonas.wettergrund@gmail.com>
2023-11-15 16:43:09 +0100

    Refactoring

---
+++ BlazorQuiz/Client/Models/QuizOptions.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorQuiz.Client.Models
+{
+    public class QuizOptions
+    {
+        public double Timer { get; set; } = 60;
+        public string QuizTitle { get; set; }
+        public bool HasTimer { get; set; }
+        public bool MediaQuestion { get; set; }
+        public bool IsValidated { get; set; }
+        public bool HasQuestion { get; set; }
+      
+
+        public void ResetToDefault()
+        {
+            MediaQuestion = false;
+        }
+    }
+}
--- BlazorQuiz/Shared/ViewModels/NewQuestionViewModel.cs
+++ BlazorQuiz/Shared/ViewModels/NewQuestionViewModel.cs
@@ -2,12 +2,37 @@
 {
     public class NewQuestionViewModel
     {
-        public string QuizImageUrl { get; set; }
+        public NewQuestionViewModel()
+        {
+            
+        }
+        public NewQuestionViewModel(NewQuestionViewModel clone)
+        {
+            Question = clone.Question;
+            Answer1 = clone.Answer1;
+            Answer2 = clone.Answer2;
+            Answer3 = clone.Answer3;
+            Answer4 = clone.Answer4;
+            CorrectAnswer = clone.Answer1;
+           
+        }
+        public string QuizImageUrl { get; set; } = "";
         public string Question { get; set; }
         public string Answer1 { get; set; }
         public string Answer2 { get; set; }
         public string Answer3 { get; set; }
         public string Answer4 { get; set; }
         public string CorrectAnswer { get; set; }
+
+        public void ResetToDefault()
+        {
+            Question = string.Empty;
+            Answer1 = string.Empty;
+            Answer2 = string.Empty;
+            Answer3 = string.Empty;
+            Answer4 = string.Empty;
+            QuizImageUrl = string.Empty;
+        }
+
     }
 }
