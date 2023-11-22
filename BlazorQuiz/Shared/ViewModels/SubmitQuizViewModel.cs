7920e49708b150669295017d45ed75348fb6d592
Jonas Wettergrund <jonas.wettergrund@gmail.com>
2023-11-22 10:51:03 +0100

    Fixed API

---
+++ BlazorQuiz/Shared/ViewModels/SubmitQuizViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorQuiz.Shared.ViewModels
+{
+    public class SubmitQuizViewModel
+    {
+    
+
+        public int gameId { get; set; }
+        public List<GuessCheckViewModel> guesses { get; set; }
+    }
+}
