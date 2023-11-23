bb16c3b935b79d7f955f1ad1530fd2d951e88273
Daniel Berkowicz <daniel.berkowicz1@gmail.com>
2023-11-23 10:57:12 +0100

    commented in IGameService to make it easier to read what it does

--- BlazorQuiz/Server/Services/IGameService.cs
+++ BlazorQuiz/Server/Services/IGameService.cs
@@ -6,16 +6,25 @@ namespace BlazorQuiz.Server.Services
 {
     public interface IGameService
     {
+        // [HttpGet("{id}")]
         Task<GameWithQuizViewModel> GetQuizByPublicIdAsync(string id);
+
+        // [HttpPost("create")]
         Task<QuizModel> CreateQuizAsync(string title, List<NewQuestionViewModel> questions, int seconds, string userId);
+
+        // [HttpPost("newgame/{quizId}")]
         Task<QuizViewModel> CreateNewGameAsync(string quizId, string userId);
-        Task<bool> UpdateGameAsync(int gameId, string gameState);
-        
+
+        // [HttpPost("guess")]
         Task<bool> CheckGuess(GuessCheckViewModel guess);
 
+        // [HttpPut("gameresult")]
+        Task<bool> UpdateGameAsync(int gameId, string gameState);
+        // [HttpPut("gameresult")]
         Task<UserQuizModel> FinishedGame(UserQuizModel quiz, List<GuessCheckViewModel> guesses);
-        
+        // [HttpPut("gameresult")]
         UserQuizModel FindUserQuiz(int id);
+        // [HttpPut("gameresult")]
         List<QuestionModel> FindQuestionsByQuizRef(string quizRef);
 
 
